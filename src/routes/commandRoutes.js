3e1f53af69635aa06813bb4b32cfe8d042248df4
kaushaleshshukla <malviyankaushalesh@gmail.com>
2020-04-20 10:24:23 +0530

    Added db for each route

--- src/routes/commandRoutes.js
+++ src/routes/commandRoutes.js
@@ -51,7 +51,7 @@ let router = function(){
                     if(req.body.PX){
                         TTL += 1*req.body.PX;
                     }
-                    console.log(TTL);
+
                 }
 
                 if(TTL==-1 && hasKey && req.body.KEEPTTL){
@@ -188,7 +188,7 @@ let router = function(){
                 
                 }
                 if(!hasKey){
-                    res.send("Nila");
+                    res.send("Nil");
                 }
                 else{
                     let obj = map.get(req.query.key);
@@ -273,123 +273,260 @@ let router = function(){
     
     commandRouter.route('/zadd')
         .post( (req, res) =>{
-            const hasKey = map.has(req.body.key);
-            if(hasKey){
+            (async function(){
+                let hasKey = map.has(req.body.key);
 
-                if(!zvalues.has(req.body.key))
-                    res.send("Error");
+                //dynamic loading of data from database
+                if(!hasKey){
+                    //connecting database
+                    let connection = await MongoClient.connect(url);
 
-                
-                else{
-                    // checking for existance of current value in corresponding key
-                    if(zvalues.get(req.body.key).has(req.body.value)){
-                        // element found with same value
-                        if(req.body.type=='XX' || req.body.type=='None'){
-                            let score = 1*zvalues.get(req.body.key).get(req.body.value);
+                    //getting database
+                    let db = connection.db(dbName);
 
-                            //deleting old instance of same value
-                            zvalues.get(req.body.key).delete(req.body.value);
-                            map.get(req.body.key).delete([score, req.body.value]);
+                    let keyTable = db.collection(tableName);
 
-                            //reinserting the element
-                            zvalues.get(req.body.key).add(1*req.body.score, req.body.value);
-                            map.get(req.body.key).push([1*req.body.score, req.body.value]);
-                            res.send("1");
+                    let result = await keyTable.findOne({"key" : req.body.key});
+
+                    if(result && result.set){
+                        let currentSet = SortedSet();
+                        let currentMap = FastMap();
+                        for(let i=0; i<result.value.length; i++){
+                            currentSet.push([1*result.value[i].score, result.value[i].member]);
+                            currentMap.add(1*result.value[i].score, result.value[i].member);
                         }
-
-                        else{
-                            res.send("nil");
-                        }
+                        map.add(currentSet, req.body.key);
+                        zvalues.add(currentMap, req.body.key);
+                        hasKey = true;
                     }
-
-                    else{
-                        // element is not present in set
-                        if(req.body.type=='XX'){
-                            res.send('nil');
-                        }
-                        else{
-                            //inserting the element
-                            zvalues.get(req.body.key).add(1*req.body.score, req.body.value);
-                            map.get(req.body.key).push([1*req.body.score, req.body.value]);
-                            res.send("1");
-                        }
+                    else if(result && !result.set){
+                        hasKey = true;
                     }
-                    
+                    connection.close();
                 }
-            }
-            else{
-                //generating a new SortedSet
-                let currentSet = SortedSet();
-                currentSet.push([1*req.body.score, req.body.value]);
+                if(hasKey){
 
-                //generating a new FastMap
-                let currentMap = FastMap();
-                currentMap.add(1*req.body.score, req.body.value);
+                    if(!zvalues.has(req.body.key))
+                        res.send("Error");
 
-                //inserting element
-                map.add(currentSet, req.body.key);
-                zvalues.add(currentMap, req.body.key);
+                    
+                    else{
+                        // checking for existance of current value in corresponding key
+                        if(zvalues.get(req.body.key).has(req.body.value)){
+                            // element found with same value
+                            if(req.body.type=='XX' || req.body.type=='None'){
+                                let score = 1*zvalues.get(req.body.key).get(req.body.value);
 
-                res.send("1");
-            }
+                                //deleting old instance of same value
+                                zvalues.get(req.body.key).delete(req.body.value);
+                                map.get(req.body.key).delete([score, req.body.value]);
+
+                                //reinserting the element
+                                zvalues.get(req.body.key).add(1*req.body.score, req.body.value);
+                                map.get(req.body.key).push([1*req.body.score, req.body.value]);
+                                res.send("1");
+
+                                //update in database
+                                //connecting database
+                                let connection = await MongoClient.connect(url);
+            
+                                //getting database
+                                let db = connection.db(dbName);
+            
+                                let keyTable = db.collection(tableName);
+            
+                                await keyTable.updateOne( {"key" : req.body.key}, 
+                                {"$set" : {"value.$[elem].score" : 1*req.body.score} },
+                                {
+                                    multi : false,
+                                    arrayFilters : [ {"elem.member" : req.body.value} ]
+                                });
+                                connection.close();
+                            }
+
+                            else{
+                                res.send("nil");
+                            }
+                        }
+
+                        else{
+                            // element is not present in set
+                            if(req.body.type=='XX'){
+                                res.send('nil');
+                            }
+                            else{
+                                //inserting the element
+                                zvalues.get(req.body.key).add(1*req.body.score, req.body.value);
+                                map.get(req.body.key).push([1*req.body.score, req.body.value]);
+                                res.send("1");
+
+                                data = {
+                                    member : req.body.value,
+                                    score : 1*req.body.score
+                                };
+                                //update in database
+                                //connecting database
+                                let connection = await MongoClient.connect(url);
+            
+                                //getting database
+                                let db = connection.db(dbName);
+            
+                                let keyTable = db.collection(tableName);
+            
+                                await keyTable.updateOne({"key" : req.body.key},
+                                {"$push" : {"value" : data}});
+                                
+                                connection.close();
+                            }
+                        }
+                        
+                    }
+                }
+                else{
+                    //generating a new SortedSet
+                    let currentSet = SortedSet();
+                    currentSet.push([1*req.body.score, req.body.value]);
+
+                    //generating a new FastMap
+                    let currentMap = FastMap();
+                    currentMap.add(1*req.body.score, req.body.value);
+
+                    //inserting element
+                    map.add(currentSet, req.body.key);
+                    zvalues.add(currentMap, req.body.key);
+
+                    res.send("1");
+
+                    data = {
+                        member : req.body.value,
+                        score : 1*req.body.score
+                    };
+
+                    entry = {
+                        key : req.body.key,
+                        set : true,
+                        value : [data]
+                    };
+                    //update in database
+                    //connecting database
+                    let connection = await MongoClient.connect(url);
+
+                    //getting database
+                    let db = connection.db(dbName);
+
+                    let keyTable = db.collection(tableName);
+
+                    await keyTable.insertOne(entry);
+                    
+                    connection.close();
+                }
+            })();
         });
 
     commandRouter.route('/zrank')
         .get( (req, res) =>{
-            if(map.has(req.query.key) && zvalues.has(req.query.key) && zvalues.get(req.query.key).has(req.query.member)){
-                //getting index
-                let index = map.get(req.query.key).indexOf([1*zvalues.get(req.query.key).get(req.query.member), req.query.member]);
-                data = {
-                    index : index
+            (async function(){
+                if(!map.has(req.query.key)){
+                    
+                    //dynamic loading from database
+                    //connecting database
+                    let connection = await MongoClient.connect(url);
+
+                    //getting database
+                    let db = connection.db(dbName);
+
+                    let keyTable = db.collection(tableName);
+
+                    let result = await keyTable.findOne({"key" : req.query.key});
+                    if(result && result.set){
+                        let currentSet = SortedSet();
+                        let currentMap = FastMap();
+                        for(let i=0; i<result.value.length; i++){
+                            currentSet.push([1*result.value[i].score, result.value[i].member]);
+                            currentMap.add(1*result.value[i].score, result.value[i].member);
+                        }
+                        map.add(currentSet, req.query.key);
+                        zvalues.add(currentMap, req.query.key);
+                    }
+                    connection.close();
                 }
-                res.send(data);
-            }
-            else{
-                //element not present
-                res.send("nil");
-            }
+                if(map.has(req.query.key) && zvalues.has(req.query.key) && zvalues.get(req.query.key).has(req.query.member)){
+                    //getting index
+                    let index = map.get(req.query.key).indexOf([1*zvalues.get(req.query.key).get(req.query.member), req.query.member]);
+                    data = {
+                        index : index
+                    }
+                    res.send(`${data.index}`);
+                }
+                else{
+                    //element not present
+                    res.send("nil");
+                }
+            })();
         });
 
     commandRouter.route('/zrange')
         .get( (req, res) =>{
-            console.log("inside zrange");
-            console.log(req.query);
-            if(zvalues.has(req.query.key)){
-                let start = 1*req.query.start;
-                let stop = 1*req.query.stop;
-                let len = map.get(req.query.key).length;
-                if(start<0)
-                    start = len+start;
-                if(stop<0)
-                    stop = len+stop;
-                stop++;
-                if(start>=len || stop>len || start>=stop || start<0 || stop<=0){
-                    let list = [];
-                    res.send(list);
-                }
-                else{
-                    let data = map.get(req.query.key).slice(start, stop);
-                    let list = [];
-                    console.log(start);
-                    console.log(stop);
-                    console.log(data);
-                    if(req.query.WITHSCORES){
-                        for(let i=0; i<data.length; i++){
-                            list.push([data[i][1], data[i][0]]);
+            (async function(){ 
+                if(!map.has(req.query.key)){
+                    
+                    //dynamic loading from database
+                    //connecting database
+                    let connection = await MongoClient.connect(url);
+
+                    //getting database
+                    let db = connection.db(dbName);
+
+                    let keyTable = db.collection(tableName);
+
+                    let result = await keyTable.findOne({"key" : req.query.key});
+
+                    if(result && result.set){
+                        let currentSet = SortedSet();
+                        let currentMap = FastMap();
+                        for(let i=0; i<result.value.length; i++){
+                            currentSet.push([1*result.value[i].score, result.value[i].member]);
+                            currentMap.add(1*result.value[i].score, result.value[i].member);
                         }
+                        map.add(currentSet, req.query.key);
+                        zvalues.add(currentMap, req.query.key);
+                    }
+                    connection.close();
+                }
+                if(zvalues.has(req.query.key)){
+                    let start = 1*req.query.start;
+                    let stop = 1*req.query.stop;
+                    let len = map.get(req.query.key).length;
+                    if(start<0)
+                        start = len+start;
+                    if(stop<0)
+                        stop = len+stop;
+                    stop++;
+                    if(start>=len || stop>len || start>=stop || start<0 || stop<=0){
+                        let list = [];
+                        res.send(list);
                     }
                     else{
-                        for(let i=0; i<data.length; i++){
-                            list.push([data[i][1]]);
+                        let data = map.get(req.query.key).slice(start, stop);
+                        let list = [];
+                        if(req.query.WITHSCORES){
+                            for(let i=0; i<data.length; i++){
+                                list.push([data[i][1], data[i][0]]);
+                            }
                         }
+                        else{
+                            for(let i=0; i<data.length; i++){
+                                list.push([data[i][1]]);
+                            }
+                        }
+                        res.send(list);
                     }
+                }
+                else{
+                    let list = [];
                     res.send(list);
                 }
-            }
-            else{
-                let list = [];
-                res.send(list);
-            }
+            })();
         });
 
     // Page not found
